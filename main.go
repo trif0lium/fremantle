dc97f21874b61c0f1aeda5d9da1b34862a573b76
Five <five@railway.app>
2023-05-10 12:27:32 +0800

    main.go: GET /hostname

--- main.go
+++ main.go
@@ -13,5 +13,12 @@ func main() {
 	e.GET("/health", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]bool{"ok": true})
 	})
+	e.GET("/hostname", func(c echo.Context) error {
+		hostname, err := os.Hostname()
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, map[string]string{"hostname": hostname})
+	})
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
 }
